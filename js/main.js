d4fe613d3ef130af80081b9ca7f832f20a6c2c89
Daniel Perez <danielperezf98@gmail.com>
2023-09-19 02:15:17 +0000

    create object of form inputs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,20 @@
 const $url = document.querySelector('#url');
 const $img = document.querySelector('img');
+const $form = document.querySelector('#entry-form');
 
 $url.addEventListener('input', function (event) {
   $img.setAttribute('src', event.target.value);
 });
 
-const $form = document.querySelector('#entry-form');
-
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
 
   const obj = {
+    entryId: data.nextEntryId,
     title: $form.elements.title.value,
     photoUrl: $form.elements.url.value,
     notes: $form.elements.notes.value,
   };
   return obj;
+  //  console.log(obj);
 });
