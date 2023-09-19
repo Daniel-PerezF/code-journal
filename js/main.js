20425bf39fcaecb71cfa010a6fc762513d751ba7
Daniel Perez <danielperezf98@gmail.com>
2023-09-19 22:10:10 +0000

    added event listener to the doc that generates dom tree #2

--- js/main.js
+++ js/main.js
@@ -21,3 +21,40 @@ $form.addEventListener('submit', function (event) {
   data.nextEntryId++;
   $form.reset();
 });
+
+const $ul = document.querySelector('ul');
+
+function renderEntry(entry) {
+  const $li = document.createElement('li');
+  $li.setAttribute('class', 'row');
+  $ul.append($li);
+
+  const $div = document.createElement('div');
+  $div.setAttribute('class', 'column-half');
+  $li.append($div);
+
+  const $img = document.createElement('img');
+  $img.setAttribute('src', entry.photoUrl);
+  $div.append($img);
+
+  const $div2 = document.createElement('div');
+  $div2.setAttribute('class', 'column-half');
+  $li.append($div2);
+
+  const $title = document.createElement('h3');
+  $div2.append($title);
+  $title.textContent = entry.title;
+
+  const $notes = document.createElement('p');
+  $notes.textContent = entry.notes;
+  $div2.append($notes);
+
+  return $li;
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+  for (let i = 0; i < data.entries.length; i++) {
+    const newLi = renderEntry(data.entries[i]);
+    $ul.append(newLi);
+  }
+});
